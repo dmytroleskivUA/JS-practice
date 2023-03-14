e1a5f79eca45104bebf71eccc4727c34efad653d
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-03-14 19:00:37 +0000

    first commit

---
+++ js/var.js
@@ -0,0 +1,19 @@
+const x = 5;
+const age = 24;
+const toatalPrice = 224;
+const userName = "Chelsy";
+const massege = "Hello World";
+const isOpen = true;
+const shoulConfirm = false;
+
+const type = typeof isOpen;
+
+console.log(type);
+
+console.log("Before");
+
+alert("HI");
+
+console.log("After");
+
+console.log(Math.PI);
