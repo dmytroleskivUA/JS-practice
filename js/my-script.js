9f138baf4d7c2d651ead78098b4e69e0d6ca8bf9
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-03-22 21:23:57 +0100

    Update my-script.js

--- js/my-script.js
+++ js/my-script.js
@@ -34,7 +34,21 @@
   |==============================================================================|
 **/
 
-// TODO
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  // Change code below this line
+let totalPrice = pricePerDroid * orderedQuantity;
+
+if (totalPrice > customerCredits) {
+message = "Insufficient funds!";
+} else {
+  customerCredits -= totalPrice;
+  message = You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left;
+}
+  
+  // Change code above this line
+  return message;
+}
 
 // ❗️❗️❗️ Оголошення змінних
 
